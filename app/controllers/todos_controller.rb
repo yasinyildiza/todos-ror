8938d2cdc4fdb03856f3e112aa64f75a52bafdfa
Yasin YILDIZ <yasin.yildiz@xapo.com>
2022-04-02 18:47:23 +0300

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,49 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to @todo
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todo_params)
+      redirect_to @todo
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    redirect_to todos_path, status: :see_other
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :body)
+    end
+end
---
+++ app/models/todo.rb
@@ -0,0 +1,4 @@
+class Todo < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
